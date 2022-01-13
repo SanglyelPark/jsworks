72b6bd87f93a902331a0f9c77b4665c1a15b7491
slpark8888 <slpark8888@gmail.com>
2022-01-13 12:59:14 +0900

    day10 자바스크립트

---
+++ day10/js/student.js
@@ -0,0 +1,13 @@
+//학생의 학년, 반, 이름 정보를 출력
+var nameing = "추신수";  //name이라는 변수 선언
+
+var grade = 2; // 선언과 동시에 초기화
+// var class; 예약어는 변수로 사용할 수 없다.
+var schoolClass = 3; // 
+
+// document.write(name + '<br>');
+// document.write(grade + '<br>');
+// document.write(schoolClass + '<br>');
+
+document.write('<p><span>' + nameing +'</span>'+ " 학생은 " 
+        + grade + "학년 " + schoolClass + "반입니다." +'</p>');
